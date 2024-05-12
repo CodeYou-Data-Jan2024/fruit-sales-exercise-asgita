10fcd73a51707ea7055023eb39f1e98ec2e5d01e
gita <asgita1332@gmail.com>
2024-05-12 14:24:17 -0400

    change4

--- fruitsales.py
+++ fruitsales.py
@@ -5,5 +5,5 @@ df = pd.DataFrame(data, columns=["Apples","Bananas"], index=data["Index"])
 print(df)
 
 df.to_csv("fruit.csv", sep=',')
-#df = pd.read_csv('fruit.csv')
-#print(df.to_string()) 
+df = pd.read_csv('fruit.csv')
+print(df.to_string()) 
